31f3564a386c87959b0419cdbdadb239e9f5cdbb
chrishtatu <ctatu@trinity.edu>
2021-03-16 11:54:07 -0500

    stuff from last class

--- server/app/edu/trinity/videoquizreact/controllers/GroupWork.scala
+++ server/app/edu/trinity/videoquizreact/controllers/GroupWork.scala
@@ -17,44 +17,63 @@ class GroupWork @Inject()(cc: ControllerComponents) extends AbstractController(c
     Ok(views.html.PlayTemplate(names))
   }
 
-  def table = Action {
-    
-    val rawData = """ 
-                team                won   lost
-                AtlantaHawks         28   54 
-                BostonCeltics        36   46 
-                NOrleansHornets      41   41 
-                ChicagoBulls         23   59 
-                ClevelandCavaliers   35   47 
-                DallasMavericks      52   30 
-                DenverNuggets        43   39 
-                DetroitPistons       54   28 
-                GSWarriors           37   45 
-                HoustonRockets       45   37 
-                IndianaPacers        61   21 
-                LAClippers           28   54 
-                LALakers             56   26 
-                MiamiHeat            42   40 
-                MilwaukeeBucks       41   41 
-                MinnesotaT-wolves    58   24 
-                NJNets               47   35 
-                NYKnicks             39   43 
-                OrlandoMagic         21   61 
-                Philadelphia76ers    33   49 
-                PhoenixSuns          29   53 
-                PortlandTrailBlazers 41   41 
-                SacramentoKings      55   27 
-                SanAntonioSpurs      57   25 
-                SeattleSupersonics   37   45 
-                TorontoRaptors       33   49 
-                UtahJazz             42   40 
-                MemphisGrizzlies     50   32 
-                WashingtonWizards    25   57 """
-    val filteredData = rawData.trim().split('\n').map((data) => data.split(/\s+/));
-
-                val table = List(List())
-    Ok(views.html.PlayTemplate(table))
+  def colorTemplate = Action {
+    Ok(views.html.getFormTemplate())
   }
+
+  def validateColor(username: String, color: String) = Action {
+    Ok(s"username is : $username, Fav Color is: $color")
+  }
+
+  def validatePost() = Action { request =>
+    val postVals = request.body.asFormUrlencoded 
+    postVals.map { args =>
+      val username = args("username")
+    }
+  }
+
+  def DisplayColors(name: String, color: String) = Action {
+    Ok(views.html.DisplayColors(name, color))
+  }
+
+ // def table = Action {
+    
+    // val rawData = """ 
+    //             team                won   lost
+    //             AtlantaHawks         28   54 
+    //             BostonCeltics        36   46 
+    //             NOrleansHornets      41   41 
+    //             ChicagoBulls         23   59 
+    //             ClevelandCavaliers   35   47 
+    //             DallasMavericks      52   30 
+    //             DenverNuggets        43   39 
+    //             DetroitPistons       54   28 
+    //             GSWarriors           37   45 
+    //             HoustonRockets       45   37 
+    //             IndianaPacers        61   21 
+    //             LAClippers           28   54 
+    //             LALakers             56   26 
+    //             MiamiHeat            42   40 
+    //             MilwaukeeBucks       41   41 
+    //             MinnesotaT-wolves    58   24 
+    //             NJNets               47   35 
+    //             NYKnicks             39   43 
+    //             OrlandoMagic         21   61 
+    //             Philadelphia76ers    33   49 
+    //             PhoenixSuns          29   53 
+    //             PortlandTrailBlazers 41   41 
+    //             SacramentoKings      55   27 
+    //             SanAntonioSpurs      57   25 
+    //             SeattleSupersonics   37   45 
+    //             TorontoRaptors       33   49 
+    //             UtahJazz             42   40 
+    //             MemphisGrizzlies     50   32 
+    //             WashingtonWizards    25   57 """
+    // val filteredData = rawData.trim().split('\n').map((data) => data.split(/\s+/));
+
+    //             val table = List(List())
+    // Ok(views.html.PlayTemplate(table))
+//  }
 }
 
 
